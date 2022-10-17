ed9e2cb16e81b9aa8a6c504ccf9162364c348c9e
ouokki <ouokki.7zgui@gmail.com>
2022-10-17 23:37:28 +0100

    #2 issue : creat models

--- user-management/src/main/java/com/rect/usermanagement/UserManagementApplication.java
+++ user-management/src/main/java/com/rect/usermanagement/UserManagementApplication.java
@@ -1,5 +1,7 @@
 package com.rect.usermanagement;
 
+import com.rect.usermanagement.models.ERole;
+import com.rect.usermanagement.models.Role;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserManagementApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(UserManagementApplication.class, args);
+
     }
 
 }
