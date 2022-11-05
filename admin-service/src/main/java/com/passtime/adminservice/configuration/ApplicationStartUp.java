8d05a9f3fc10884f6314362180b060c5507ccf73
ouokki <ouokki.7zgui@gmail.com>
2022-11-05 15:38:18 +0100

    initialize some admin data at the application startup stage

---
+++ admin-service/src/main/java/com/passtime/adminservice/configuration/ApplicationStartUp.java
@@ -0,0 +1,44 @@
+package com.passtime.adminservice.configuration;
+
+import com.passtime.adminservice.entities.Admin;
+import com.passtime.adminservice.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Mohamed ouokki on 11/5/22
+ * @project eureka-server
+ */
+@Component
+public class ApplicationStartUp {
+
+    private final AdminRepository adminRepository;
+
+    private final StartupProperties startupProperties;
+
+    public ApplicationStartUp(AdminRepository adminRepository, StartupProperties startupProperties) {
+        this.adminRepository = adminRepository;
+        this.startupProperties = startupProperties;
+    }
+
+    @Bean
+    public CommandLineRunner loadData(){
+        return (args -> {
+            List<Admin> admins=adminRepository.findAll();
+            if(ObjectUtils.isEmpty(admins)){
+                this.adminRepository.save(Admin.builder()
+                        .username(startupProperties.getUsername())
+                        .password(startupProperties.getPassword())
+                        .roles(Arrays.asList("ADMIN"))
+                        .status(startupProperties.getStatus())
+                        .build());
+            }
+        });
+    }
+}
--- auth-service/src/main/java/com/passtime/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/passtime/authservice/AuthServiceApplication.java
@@ -3,12 +3,13 @@ package com.passtime.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableZuulServer
-@EnableEurekaClient
+@EnableZuulProxy // Enables the server to be a Zuul server
+@EnableEurekaClient // Annotation for clients to enable Eureka discovery configuration
 @EnableFeignClients
 public class AuthServiceApplication {
 
